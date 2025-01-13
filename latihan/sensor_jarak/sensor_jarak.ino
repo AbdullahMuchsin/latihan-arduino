f3bea14fddf6e027d3011598f79cbbc5b8f51db1
AbdullahMuchsin <abdullahmuchsin96@gmail.com>
2025-01-13 23:48:50 +0700

    Update Kode

--- latihan/sensor_jarak/sensor_jarak.ino
+++ latihan/sensor_jarak/sensor_jarak.ino
@@ -2,14 +2,32 @@
 
 NewPing sensor(8,9,30);
 
+const int pinLed = 7;
+const int pinBuz = 6;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(pinLed, OUTPUT);
+  pinMode(pinBuz, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int nilaiSensor = sensor.ping_cm();
-
   Serial.println(nilaiSensor);
+
+  if(nilaiSensor <= 10 && nilaiSensor != 0) {
+    digitalWrite(pinLed, HIGH);
+    delay(50);
+    digitalWrite(pinLed, LOW);
+    delay(50);
+  } else {
+    digitalWrite(pinLed, HIGH);
+    digitalWrite(pinBuz, HIGH);
+    delay(1000);
+    digitalWrite(pinLed, LOW);
+    digitalWrite(pinBuz, LOW);
+    delay(1000);
+  }
 }
