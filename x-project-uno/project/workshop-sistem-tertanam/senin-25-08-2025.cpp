8aa6396719f3e6f82afc2751c6618b56b882df27
Abdullah Muchsin <Abdullahmuchsin96@gmail.com>
2025-08-28 00:55:18 +0700

    Update Kode

--- x-project-uno/project/workshop-sistem-tertanam/senin-25-08-2025.cpp
+++ x-project-uno/project/workshop-sistem-tertanam/senin-25-08-2025.cpp
@@ -125,3 +125,70 @@ void loop()
     digitalWrite(6, LOW);
     delay(100);
 }
+
+// 7 : My Case
+int pinLed = 6;
+int button = 2;
+int bacaButton, lastButton, kondisi = 0;
+
+// Tambahan
+int button2 = 3;     // untuk LED2
+int button3 = 4;     // untuk buzzer
+int pin_led2 = 7;    // LED2
+int pin_buzzer = 10; // buzzer
+int bacaButton3, lastButton3 = HIGH, kondisi_buzzer = 0;
+
+void setup()
+{
+    pinMode(pinLed, OUTPUT);
+    pinMode(button, INPUT_PULLUP);
+
+    // tambahan
+    pinMode(button2, INPUT_PULLUP);
+    pinMode(button3, INPUT_PULLUP);
+    pinMode(pin_led2, OUTPUT);
+    pinMode(pin_buzzer, OUTPUT);
+}
+
+void loop()
+{
+    // --- Button 1 : toggle LED1 ---
+    bacaButton = digitalRead(button);
+
+    if ((bacaButton == HIGH) && (lastButton == LOW))
+    {
+        kondisi = 1 - kondisi;
+        delay(10);
+    }
+
+    lastButton = bacaButton;
+
+    if (kondisi == 1)
+    {
+        digitalWrite(pinLed, HIGH);
+    }
+    else
+    {
+        digitalWrite(pinLed, LOW);
+    }
+
+    // --- Button 2 : LED2 nyala hanya saat ditekan ---
+    if (digitalRead(button2) == LOW) // karena INPUT_PULLUP, aktif saat LOW
+        digitalWrite(pin_led2, HIGH);
+    else
+        digitalWrite(pin_led2, LOW);
+
+    // --- Button 3 : toggle buzzer ---
+    bacaButton3 = digitalRead(button3);
+    if ((bacaButton3 == LOW) && (lastButton3 == HIGH))
+    {
+        kondisi_buzzer = 1 - kondisi_buzzer;
+        delay(10);
+    }
+    lastButton3 = bacaButton3;
+
+    if (kondisi_buzzer == 1)
+        digitalWrite(pin_buzzer, HIGH);
+    else
+        digitalWrite(pin_buzzer, LOW);
+}
